fff96072cc1be0dca2d50750d724f8a802c95124
Saikaku-69 <grnmnsta@gmail.com>
2024-08-08 01:23:50 +0900

    main作成

---
+++ FolderApp/FolderApp/FolderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FolderAppApp.swift
+//  FolderApp
+//
+//  Created by cmStudent on 2024/08/07.
+//
+
+import SwiftUI
+
+@main
+struct FolderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
